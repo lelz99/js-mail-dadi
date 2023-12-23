9c9163804dace21f2bb5c35e101bba96b6034108
lelz99 <emanuelesimonetti99@gmail.com>
2023-12-23 13:04:06 +0100

    corretto esercizio mail

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
 //Esercizio Login
-const inputMail = document.getElementById('user-mail')
+
+const emailsLoggate = ['es99@gmail.com','se99@gmail.com','es1999@gmail.com']
+
+const inputEmail = document.getElementById('user-mail')
 const button = document.getElementById('button-invia')
-const mailLoggate = ['es99@gmail.com','se99@gmail.com','es1999@gmail.com']
 const paragraph = document.getElementById('risultato-mail')
 
+button.addEventListener('click', function(){
+    const userEmail = inputEmail.value.trim();
 
-    button .addEventListener('click', function() {
+    if(!userEmail){
+        alert('non hai inserito una mail')
+        return
+    }
 
-        if (inputMail.value == mailLoggate[0] || inputMail.value == mailLoggate[1] || inputMail.value == mailLoggate[2]){ 
-           text = 'Mail registrata'
-        } else {
-            text = 'Mail non registrata'
+    let userLoggato = false;
+    text = 'mail non registrata'
+
+    for(let i = 0; i < emailsLoggate.length; i++){
+
+        if (userEmail === emailsLoggate[i]){
+            text = 'mail registrata'
+            let = userLoggato = true
         }
-        paragraph.innerText = text
-    })
+    }
+    paragraph.innerText = text
+});
 
 
 ///////////////////////////////////////////////////////////////////
@@ -28,21 +40,21 @@ const buttonNumbers = document.getElementById('button')
 
 buttonNumbers .addEventListener('click', function(){
 
-    // Genero i numeri random
-    const firstPlayer = Math.floor(Math.random() *6) + 1
-    const secondPlayer = Math.floor(Math.random() *6) + 1
+// Genero i numeri random
+ const firstPlayer = Math.floor(Math.random() *6) + 1
+ const secondPlayer = Math.floor(Math.random() *6) + 1
 
-    // Decido cosa succede in base al risultato dei numeri random
+ // Decido cosa succede in base al risultato dei numeri random
     if (firstPlayer > secondPlayer){
-        resultGame = 'First Player Win'
-    } else if (secondPlayer > firstPlayer) {
-        resultGame = 'Second Player Win'
-    } else {
-        resultGame = 'Tie'
+        resultGame = 'First Player Win' 
+     } else if (secondPlayer > firstPlayer) {
+      resultGame = 'Second Player Win'
+     } else {
+     resultGame = 'Tie'
     }
 
-    // Stampo in pagina
-    playerOne.innerText = 'Player 1: ' + firstPlayer
-    playerTwo.innerText = 'Player 2: ' + secondPlayer
-    result.innerText = resultGame
+ // Stampo in pagina
+ playerOne.innerText = 'Player 1: ' + firstPlayer
+ playerTwo.innerText = 'Player 2: ' + secondPlayer
+ result.innerText = resultGame
 })
